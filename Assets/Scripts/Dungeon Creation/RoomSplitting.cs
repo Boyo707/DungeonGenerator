08de141a7b068467e7c34b5e267b00555a8871f5
boyd <boydvanlingen@gmail.com>
2025-07-13 23:33:54 +0200

    Changed isHorizontal Inside roomSplitting
    It will now use a enum that dictates if it is horizontal, vertical or unable to split

--- Assets/Scripts/Dungeon Creation/RoomSplitting.cs
+++ Assets/Scripts/Dungeon Creation/RoomSplitting.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RoomSplitting : MonoBehaviour
 {
     //somehow get this within the enumerator
     private bool isHorizontalSplit = false;
+
+    public enum SplitType
+    {
+        Horizontal,
+        Vertical,
+        UnAble
+    }
 
     public IEnumerator Splitting(float stepDelay, int dungeonSeed, Dungeon2 dungeonGeneration)
     {
@@ -18,6 +26,8 @@ public class RoomSplitting : MonoBehaviour
         Queue<RectInt> Q = new();
         HashSet<RectInt> discovered = new();
 
+        SplitType roomSplitType = SplitType.Horizontal;
+
 
         Q.Enqueue(dungeonGeneration.dungeonSize);
 
@@ -25,14 +35,22 @@ public class RoomSplitting : MonoBehaviour
         {
             RectInt currentRoom = Q.Dequeue();
 
-            isHorizontalSplit = !isHorizontalSplit;
+            if (roomSplitType == SplitType.Horizontal) roomSplitType = SplitType.Vertical;
+            if (roomSplitType == SplitType.Vertical) roomSplitType = SplitType.Horizontal;
+            if (roomSplitType == SplitType.UnAble) roomSplitType = SplitType.Horizontal;
+
+            //isHorizontalSplit = !isHorizontalSplit;
+
 
             //If it can split the room and it has not been discovered
-            if (!discovered.Contains(currentRoom) && CanSplitRoom(currentRoom, isHorizontalSplit, dungeonGeneration.minRoomSize))
+
+            roomSplitType = CanSplitRoom(currentRoom, roomSplitType, dungeonGeneration.minRoomSize);
+
+            if (!discovered.Contains(currentRoom) && roomSplitType != SplitType.UnAble)
             {
                 discovered.Add(currentRoom);
 
-                RectInt[] splitRooms = SplitRoom(currentRoom, isHorizontalSplit, dungeonGeneration.minRoomSize, dungeonGeneration.wallMargin);
+                RectInt[] splitRooms = SplitRoom(currentRoom, roomSplitType, dungeonGeneration.minRoomSize, dungeonGeneration.wallMargin);
 
                 //Remove the big room after it gets split
                 createdRooms.Remove(currentRoom);
@@ -47,7 +65,6 @@ public class RoomSplitting : MonoBehaviour
                     dungeonGeneration.createdRooms = createdRooms;
 
                     //assigngs the current depth color to this room
-                    //dungeonGeneration.drawColors.Add(dungeonGeneration.depthColor);
 
                     if (dungeonGeneration.generationType == GenerationType.Timed || dungeonGeneration.generationType == GenerationType.TimedStep)
                     {
@@ -57,6 +74,41 @@ public class RoomSplitting : MonoBehaviour
             }
         }
     }
+
+    public SplitType CanSplitRoom(RectInt currentRoom, SplitType splitType, int minRoomSize)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            if (splitType == SplitType.Horizontal)
+            {
+                //calculates if the minimal room size can fit in the current room split.
+                if (currentRoom.height - minRoomSize > minRoomSize)
+                {
+                    return SplitType.Horizontal;
+                }
+                else
+                {
+                    splitType = SplitType.Vertical;
+                }
+            }
+            else
+            {
+                //calculates if the minimal room size can fit in the current room split.
+                if (currentRoom.width - minRoomSize > minRoomSize)
+                {
+                    return SplitType.Vertical;
+                }
+                else
+                {
+                    splitType = SplitType.Horizontal;
+                }
+            }
+        }
+
+        //false when it's not possible to split from any side
+        return SplitType.UnAble;
+    }
+
     public bool CanSplitRoom(RectInt currentRoom, bool checkingHorizontal, int minRoomSize)
     {
         for (int i = 0; i < 2; i++)
@@ -92,6 +144,7 @@ public class RoomSplitting : MonoBehaviour
         //false when it's not possible to split from any side
         return false;
     }
+
     public RectInt[] SplitRoom(RectInt currentRoom, bool isHorizontal, int minRoomSize, int wallMargin)
     {
         RectInt[] nextRoomArray = new RectInt[2];
@@ -124,4 +177,38 @@ public class RoomSplitting : MonoBehaviour
 
         return nextRoomArray;
     }
+    public RectInt[] SplitRoom(RectInt currentRoom, SplitType splitType, int minRoomSize, int wallMargin)
+    {
+        RectInt[] nextRoomArray = new RectInt[2];
+
+        //Checks which direction it has been split in
+        if (splitType == SplitType.Horizontal)
+        {
+            int splitPosition = Random.Range(minRoomSize, currentRoom.height - minRoomSize);
+
+            Vector2Int roomSize1 = new(currentRoom.width, splitPosition);
+            Vector2Int roomSize2 = new(currentRoom.width, currentRoom.height - splitPosition);
+
+            //Bottom Room Rect
+            nextRoomArray[0] = new(currentRoom.x, currentRoom.y, roomSize1.x, roomSize1.y + wallMargin);
+            //Top Room Rect
+            nextRoomArray[1] = new(currentRoom.x, currentRoom.y + roomSize1.y, roomSize2.x, roomSize2.y);
+        }
+        else
+        {
+            int splitPosition = Random.Range(minRoomSize, currentRoom.width - minRoomSize);
+
+            Vector2Int roomSize1 = new(splitPosition, currentRoom.height);
+            Vector2Int roomSize2 = new(currentRoom.width - splitPosition, currentRoom.height);
+
+            //Left Room Rect
+            nextRoomArray[0] = new(currentRoom.x, currentRoom.y, roomSize1.x + wallMargin, roomSize1.y);
+            //Right Room Rect
+            nextRoomArray[1] = new(roomSize1.x + currentRoom.x, currentRoom.y, roomSize2.x, roomSize2.y);
+        }
+
+        return nextRoomArray;
+    }
+
+
 }
